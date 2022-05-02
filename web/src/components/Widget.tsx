44a0e297593eb7983fdfcac09ce3feaf57d772cb
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-05-02 17:36:10 -0300

    feat: add accessibility to widget component with headlessui lib

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,17 +1,19 @@
 import  { ChatTeardropDots } from 'phosphor-react';
+import { Popover } from '@headlessui/react'; 
 
 export function Widget() {
   return (
-    <div className="absolute bottom-5 right-5">
-      <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+    <Popover className="absolute bottom-5 right-5">
+      <Popover.Panel>Hi, this component is accessible from! :3</Popover.Panel>
+      <Popover.Button  className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6"/>
       
       <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-700 ease-linear">
         <span className="pl-2"></span>
         Feedback
       </span>
-      </button>
+      </Popover.Button>
 
-    </div>
+    </Popover>
     )
 }
